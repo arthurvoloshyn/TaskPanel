928501aea96bb478cfeac907166ffc74eb9beb01
Artur <grandfaraon93@gmail.com>
2019-11-24 13:06:06 +0200

    notes update

--- src/redux/reducers/notes.js
+++ src/redux/reducers/notes.js
@@ -4,7 +4,9 @@ import { ADD_NOTE } from "../actions/notes";
 let initState = load({ namespace: 'todo-list' });
 
 if (!initState || !initState.notes || !initState.notes.length) {
-  initState.notes = ["note 1", "note 2", "note 3", "note 4"];
+  initState = {
+    notes: ["note 1", "note 2", "note 3", "note 4"],
+  }
 }
 
 export default (state = initState.notes, action) => {
