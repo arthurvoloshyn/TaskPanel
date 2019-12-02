c9e0734295dae334e863a36fd12a4b8ac5085a2e
Artur <grandfaraon93@gmail.com>
2019-12-02 18:23:43 +0200

    Updates

--- precache-manifest.550c684f72d44fa8a67c8c7642a783e8.js
+++ precache-manifest.550c684f72d44fa8a67c8c7642a783e8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/TaskPanel/static/js/runtime~main.83a36643.js"
   },
   {
-    "revision": "c94798ad22234ced2fd5",
-    "url": "/TaskPanel/static/js/main.c94798ad.chunk.js"
+    "revision": "29e835277e25d1536a49",
+    "url": "/TaskPanel/static/js/main.29e83527.chunk.js"
   },
   {
     "revision": "3cf1e0a16ba8939110ec",
     "url": "/TaskPanel/static/js/1.3cf1e0a1.chunk.js"
   },
   {
-    "revision": "c94798ad22234ced2fd5",
+    "revision": "29e835277e25d1536a49",
     "url": "/TaskPanel/static/css/main.5497a557.chunk.css"
   },
   {
-    "revision": "f3f14dd163efda9e64ff688dcc114291",
+    "revision": "e137b815d1fa085c5be0aba22ade817c",
     "url": "/TaskPanel/index.html"
   }
 ];
